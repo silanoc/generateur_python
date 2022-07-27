29faa835a19af7c6f4535fa0bf6bdffe16f5a1e4
silanoc <silanoc@gmail.com>
2022-07-27 11:46:07 +0200

    construction class avec Red/Green

---
+++ generateur.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+class Mon_generateur():
+    def __init__(self, nom, chemin):
+        pass
---
+++ test_generateur.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import generateur
+
+def test_class_generateur():
+    mon_generateur = generateur.Mon_generateur('nom_A', 'chemin_A')
