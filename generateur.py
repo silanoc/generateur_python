14ee2beb532f2fdbd81afb0560d336e21d085923
silanoc <silanoc@gmail.com>
2022-07-27 12:10:44 +0200

    création dossier avec red/green

--- generateur.py
+++ generateur.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import os
+
 class Mon_generateur():
     def __init__(self, nom, chemin):
-        pass
+        self.nom = nom
+        self.chemin = chemin
+        
+    def creerdossier(self, nom, chemin):
+        try:
+            os.makedirs(f'{chemin}/{nom}')
+        except FileExistsError:
+            pass
+ 
--- test_generateur.py
+++ test_generateur.py
@@ -2,6 +2,14 @@
 # coding: utf-8
 
 import generateur
+import os
+import sys
 
 def test_class_generateur():
     mon_generateur = generateur.Mon_generateur('nom_A', 'chemin_A')
+    
+def test_creation_dossier():
+    mon_generateur = generateur.Mon_generateur('nom_A', sys.path[0])
+    #nom = f'{sys.path[0]}/nom_A'
+    mon_generateur.creerdossier('nom_A', sys.path[0])
+    assert os.path.exists(f'{sys.path[0]}/nom_A') == True
